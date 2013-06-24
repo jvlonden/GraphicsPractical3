b1a0b954b330434e1e31d435bf754d5a62752716
Jordy van Londen <jordy.vanlonden@gmail.com>
2013-06-24 20:56:47 +0200

    Initial Commit

---
+++ GraphicsPractical3/GraphicsPractical3/Game1.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace GraphicsPractical3
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        // Often used XNA objects
+        private GraphicsDevice device;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private FrameRateCounter frameRateCounter;
+
+        // Game objects and variables
+        private Camera camera;
+
+        // Model
+        private Model model;
+        private Material modelMaterial;
+
+        // Quad
+        private VertexPositionNormalTexture[] quadVertices;
+        private short[] quadIndices;
+        private Matrix quadTransform;
+
+        public Game1()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+            // Create and add a frame rate counter
+            this.frameRateCounter = new FrameRateCounter(this);
+            this.Components.Add(this.frameRateCounter);
+        }
+
+        protected override void Initialize()
+        {
+            this.device = graphics.GraphicsDevice;
+            // Copy over the device's rasterizer state to change the current fillMode
+            this.device.RasterizerState = new RasterizerState() { CullMode = CullMode.None };
+            // Set up the window
+            this.graphics.PreferredBackBufferWidth = 800;
+            this.graphics.PreferredBackBufferHeight = 600;
+            this.graphics.IsFullScreen = false;
+            // Let the renderer draw and update as often as possible
+            this.graphics.SynchronizeWithVerticalRetrace = false;
+            this.IsFixedTimeStep = false;
+            // Flush the changes to the device parameters to the graphics card
+            this.graphics.ApplyChanges();
+            // Initialize the camera
+            this.camera = new Camera(new Vector3(0, 50, 100), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+
+            this.IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a SpriteBatch object
+            this.spriteBatch = new SpriteBatch(this.device);
+            // Load the "Simple" effect
+            Effect effect = this.Content.Load<Effect>("Effects/Simple");
+            // Load the model and let it use the "Simple" effect
+            this.model = this.Content.Load<Model>("Models/femalehead");
+            this.model.Meshes[0].MeshParts[0].Effect = effect;
+            // Setup the quad
+            this.setupQuad();
+
+            //set the color of the object
+            this.modelMaterial.DiffuseColor = Color.Red;
+            //set the position of the light   
+            this.modelMaterial.Light = new Vector3(50, 50, 50);
+
+            //ambient light color
+            this.modelMaterial.AmbientColor = Color.Red;
+            //ambient light intensity
+            this.modelMaterial.AmbientIntensity = 0.2f;
+
+            //specular light settings
+            this.modelMaterial.Eye = this.camera.Eye;
+            this.modelMaterial.SpecularColor = Color.White;
+            this.modelMaterial.SpecularIntensity = 2.0f;
+            this.modelMaterial.SpecularPower = 25.0f;
+
+            //apply changes for the model effect
+            this.modelMaterial.SetEffectParameters(effect);
+        }
+
+        /// <summary>
+        /// Sets up a 2 by 2 quad around the origin.
+        /// </summary>
+        private void setupQuad()
+        {
+            float scale = 50.0f;
+
+            // Normal points up
+            Vector3 quadNormal = new Vector3(0, 1, 0);
+
+            this.quadVertices = new VertexPositionNormalTexture[4];
+            // Top left
+            this.quadVertices[0].Position = new Vector3(-1, 0, -1);
+            this.quadVertices[0].Normal = quadNormal;
+            this.quadVertices[0].TextureCoordinate = new Vector2(0, 0);
+            // Top right
+            this.quadVertices[1].Position = new Vector3(1, 0, -1);
+            this.quadVertices[1].Normal = quadNormal;
+            this.quadVertices[1].TextureCoordinate = new Vector2(1, 0);
+            // Bottom left
+            this.quadVertices[2].Position = new Vector3(-1, 0, 1);
+            this.quadVertices[2].Normal = quadNormal;
+            this.quadVertices[2].TextureCoordinate = new Vector2(0, 1);
+            // Bottom right
+            this.quadVertices[3].Position = new Vector3(1, 0, 1);
+            this.quadVertices[3].Normal = quadNormal;
+            this.quadVertices[3].TextureCoordinate = new Vector2(1, 1);
+
+            this.quadIndices = new short[] { 0, 1, 2, 1, 2, 3 };
+            this.quadTransform = Matrix.CreateScale(scale) * Matrix.CreateTranslation(0.0f, -7.5f, 0.0f);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            float timeStep = (float)gameTime.ElapsedGameTime.TotalSeconds * 60.0f;
+
+            // Update the window title
+            this.Window.Title = "XNA Renderer | FPS: " + this.frameRateCounter.FrameRate;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // Clear the screen in a predetermined color and clear the depth buffer
+            this.device.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.DeepSkyBlue, 1.0f, 0);
+
+            // Get the model's only mesh
+            ModelMesh mesh = this.model.Meshes[0];
+            Effect effect = mesh.Effects[0];
+
+            // Set the effect parameters
+            effect.CurrentTechnique = effect.Techniques["Simple"];
+
+            // Matrices for 3D perspective projection
+            this.camera.SetEffectParameters(effect);
+
+            // World transform for the teapot
+            Matrix worldTransform = Matrix.CreateScale(0.5f);
+            effect.Parameters["World"].SetValue(worldTransform);
+            effect.Parameters["WorldNormal"].SetValue(Matrix.Transpose(Matrix.Invert(worldTransform)));
+
+            // Let the shader know there's no texture
+            effect.Parameters["HasTexture"].SetValue(false);
+
+            // Draw the model
+            mesh.Draw();
+
+            // Pass the texture to the shader and let it know it has to use it
+            effect.Parameters["DiffuseTexture"].SetValue(Content.Load<Texture>("Textures/CobblestonesDiffuse"));
+            effect.Parameters["HasTexture"].SetValue(true);
+
+            // World transform for the quad
+            effect.Parameters["World"].SetValue(quadTransform);
+            effect.Parameters["WorldNormal"].SetValue(Matrix.Transpose(Matrix.Invert(quadTransform)));
+
+            // Draw the quad
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.DrawUserIndexedPrimitives(
+                    PrimitiveType.TriangleList,
+                    quadVertices,
+                    0,
+                    quadVertices.Length,
+                    quadIndices,
+                    0,
+                    quadIndices.Length / 3);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GraphicsPractical3/GraphicsPractical3Content/Effects/Simple.fx
@@ -0,0 +1,156 @@
+//------------------------------------------- Defines -------------------------------------------
+
+#define Pi 3.14159265
+
+//------------------------------------- Top Level Variables -------------------------------------
+
+// Top level variables can and have to be set at runtime
+
+// Positions of the lightsource and the camera
+float3 Light, Eye;
+// Matrices for 3D perspective projection 
+float4x4 View, Projection, World, WorldNormal;
+// Light Colors
+float4 DiffuseColor, AmbientColor, SpecularColor;
+// Other lighting values
+float AmbientIntensity, SpecularPower, SpecularIntensity;
+
+Texture DiffuseTexture;
+sampler DiffuseTextureSampler = sampler_state 
+{
+	texture = <DiffuseTexture>;
+	AddressU = mirror;
+	AddressV = mirror;
+};
+bool HasTexture;
+
+//---------------------------------- Input / Output structures ----------------------------------
+
+// Each member of the struct has to be given a "semantic", to indicate what kind of data should go in
+// here and how it should be treated. Read more about the POSITION0 and the many other semantics in 
+// the MSDN library
+struct VertexShaderInput
+{
+	float4 Position3D : POSITION0;
+	float3 Normal : NORMAL0;
+	float2 TexCoords : TEXCOORD0;
+};
+
+// The output of the vertex shader. After being passed through the interpolator/rasterizer it is also 
+// the input of the pixel shader. 
+// Note 1: The values that you pass into this struct in the vertex shader are not the same as what 
+// you get as input for the pixel shader. A vertex shader has a single vertex as input, the pixel	
+// shader has 3 vertices as input, and lets you determine the color of each pixel in the triangle 
+// defined by these three vertices. Therefor, all the values in the struct that you get as input for 
+// the pixel shaders have been linearly interpolated between there three vertices!
+// Note 2: You cannot use the data with the POSITION0 semantic in the pixel shader.
+struct VertexShaderOutput
+{
+	float4 Position2D : POSITION0;
+	float3 Normal : TEXCOORD1;
+	float3 PixelPosition : TEXCOORD2;	
+	float2 TexCoords : TEXCOORD0;
+};
+
+//------------------------------------------ Functions ------------------------------------------
+
+// Implement the Coloring using normals assignment here
+float4 NormalColor(float3 normal)
+{
+	//copy the data to the output
+	float4 output;
+	output.xyz = normal	;
+	output.w = 1.0;
+	
+	return output;
+}
+
+// Implement the Procedural texturing assignment here
+float4 ProceduralColor(VertexShaderOutput input)
+{
+	//reverse the normal and save its values in iNormal
+	float3 iNormal = -input.Normal;
+	float width = 2;
+	float height = 2;
+	
+	// if the sin of the Y and X coordinates are BOTH above or below 0
+	if( (sin(Pi * input.PixelPosition.x / width) > 0 && sin(Pi * input.PixelPosition.y / height) > 0) || (sin(Pi * input.PixelPosition.x / width) <= 0 && sin(Pi * input.PixelPosition.y / height) <= 0) )
+	{
+		//color using the normal
+		return NormalColor(input.Normal);	
+	}
+	else
+	{
+		//color using the inverse
+		return NormalColor(iNormal);
+	}
+}
+
+float4 Lighting(VertexShaderOutput input)
+{
+	//calculate the vector between the lightpoint and the position of the pixel
+	float3 lightDir = normalize(Light - input.PixelPosition);
+	
+	//calculate the vector between the eye and the .....
+	float3 viewDir = normalize(Eye - input.PixelPosition);
+	//calculate the halfway vector
+	float3 h = normalize(viewDir + lightDir);	
+	
+	//calculate the cosine of the angle between the normal and the directional light and clamp it between 0 and 1;
+	float diffuse = saturate(mul(input.Normal, lightDir));
+	
+	//calculate the ambient light
+	float4 ambient = AmbientColor * AmbientIntensity;
+	
+	//calculate the intensity of the specular light at a given point
+	float4 specular = pow(saturate(mul(input.Normal, h)), SpecularPower) * SpecularIntensity;
+	
+	//combine all the lighting
+	return  ambient + DiffuseColor * diffuse + SpecularColor * specular;
+}
+
+//---------------------------------------- Technique: Simple ----------------------------------------
+
+VertexShaderOutput SimpleVertexShader(VertexShaderInput input)
+{
+	// Allocate an empty output struct
+	VertexShaderOutput output = (VertexShaderOutput)0;
+
+	// Do the matrix multiplications for perspective projection and the world transform
+	float4 worldPosition = mul(input.Position3D, World);
+    float4 viewPosition  = mul(worldPosition, View);
+	output.Position2D    = mul(viewPosition, Projection);
+	
+	output.TexCoords = input.TexCoords;
+	
+	//perform the transposed inverse world transform on the normals
+	output.Normal		 = normalize(mul(input.Normal, (float3x3)WorldNormal));
+	
+	//pass along the position data to the output (for pixel shader use)
+	output.PixelPosition = worldPosition;
+	
+	return output;
+}
+
+float4 SimplePixelShader(VertexShaderOutput input) : COLOR0
+{
+	//scale of the textures
+	float textureScale = 0.5;
+	
+	float4 color;
+	if(!HasTexture)
+		color = Lighting(input);
+	else
+		color = tex2D(DiffuseTextureSampler, input.TexCoords / textureScale);
+
+	return color;
+}
+
+technique Simple
+{
+	pass Pass0
+	{
+		VertexShader = compile vs_2_0 SimpleVertexShader();
+		PixelShader  = compile ps_2_0 SimplePixelShader();
+	}
+}
